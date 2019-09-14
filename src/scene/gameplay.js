27593e89826255dbba6f1e79108bd7f070571a04
Daniel Savage <dansvg@gmail.com>
2019-09-14 13:18:40 -0700

    3D test

--- src/scene/gameplay.js
+++ src/scene/gameplay.js
@@ -6,6 +6,7 @@ let Gameplay = function (config) {
     this.player = null;
     this.map = null;
     this.foreground = null;
+    this.three = null;
 };
 Gameplay.prototype.init = function () {
     //
@@ -19,6 +20,29 @@ Gameplay.prototype.preload = function () {
     this.load.tilemapTiledJSON('test_map', 'asset/map/test_map.json');
 };
 Gameplay.prototype.create = function () {
+
+    this.three = this.add.extern();
+
+    let camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.z = 400;
+
+    let scene = new THREE.Scene();
+
+    let geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+    let material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+
+    let mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    let renderer = new THREE.WebGLRenderer( { canvas: this.game.canvas, context: this.game.context, antialias: false } );
+    this.three.render = function (prenderer, pcamera, pcalcMatrix) {
+        mesh.rotation.x += 0.01;
+
+
+        renderer.state.reset();
+        renderer.render(scene, camera);
+    };
+
     this.player = new Player(this, 128, 64);
 
     this.map = this.add.tilemap('test_map');
@@ -34,4 +58,5 @@ Gameplay.prototype.shutdown = function () {
     this.player = null;
     this.map = null;
     this.foreground = null;
+    this.three = null;
 };
